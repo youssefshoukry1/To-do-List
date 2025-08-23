2c7170ad315e1a53d5399cc1203e92e45555f643
youssefshoukry1 <youssefshoukry1@hotmail.com>
2025-08-24 01:59:04 +0300

    update project

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 export default function Footer() {
   const fadeIn = {
     hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: [0.42, 0, 0.58, 1] } },
   };
 
   const whatsappNumber = "201204470794";
