dda1395251708f0519f8599c93fb7ca7fcad31b4
youssefshoukry1 <youssefshoukry1@hotmail.com>
2025-08-26 19:34:46 +0300

    update project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,13 +12,15 @@ const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"]
 export const metadata: Metadata = {
   title: "To-Do",
   description: "To-Do-List",
+  icons: {
+    icon: "/List.webp", // هيظهر بدل أي favicon افتراضي
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <Head>
-  <link rel="icon" href="/List.webp" />
   <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
